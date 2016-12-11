f6277c33b1a6c5287f2857381da14e4ef89f7613
Sonya Roy <sonyaroy75@gmail.com>
2016-12-12 03:35:05 +0530

    Add files via upload

---
+++ proxy-version/market.py
@@ -0,0 +1,130 @@
+from evony import *
+import time
+import os
+import sys
+from threading import Thread
+from actionfactory.builder import *
+from actionfactory.quest import *
+from actionfactory.items import *
+itemsarray={'player.box.gambling.wood':1,'player.box.gambling.food':0,'player.box.gambling.stone':2,'player.box.gambling.iron':3}
+reslist=['food','wood','stone','iron']
+def createacc(server,prioritylist,useclient=None,proxy=None,proxytype='HTTP',callback=None,timeout=30):
+	global restypes,quests,itemsarray,reslist
+	try:
+		invalidname=False
+		t=[]
+		for p in prioritylist:
+			if p not in t:
+				t.append(p)
+		prioritylist=t
+		prioritylist=[reslist[(int(j)-1)] for j in prioritylist if int(j)!=0]
+		if prioritylist==[]:
+			callback("8||INVALIDCHOICE")
+			return
+		if proxy!=None:
+			try:
+				proxyhost=proxy.split(':')[0]
+				proxyport=int(proxy.split(':')[1])
+				setproxy=True
+			except:
+				proxyhost=''
+				proxyport=0
+				setproxy=False
+		else:
+			proxyhost=''
+			proxyport=0
+			setproxy=False
+		if useclient==None:
+			x=Client(server,setproxy=setproxy,proxyhost=proxyhost,proxyport=proxyport,proxytype=proxytype,callback=callback,timeout=timeout)
+		else:
+			x=useclient
+		y=x.registernewplayer()
+		callback("|9|9")
+		builder=Builder(x)
+		castleid=y['data']['player']['castles'][0]['id']
+		quest=Quest(x,castleid)
+		items=Item(x,castleid)
+		items.useitem('player.box.present.2')
+		x.client.sendmessage('common.addToFavorites',{})
+		res=x.responsehandler('common.addToFavorites')
+		builder.createbuilding(castleid,0,1)
+		quest.completequest(1)
+		quest.completequest(226)
+		quest.completequest(535)
+		builder.createbuilding(castleid,1,23)
+		time.sleep(.5)
+		builder.speedup(castleid,'consume.2.a',1)
+		while True:
+			res=x.responsehandler('server.BuildComplate',checkok=False)
+			if res['data']['buildingBean']['endTime']==0.0:
+				break
+		result=[]
+		v=items.useitem('player.box.gambling.3')
+		if v['data']['itemBeans'][0]['id'] in itemsarray:
+			result.append(v['data']['itemBeans'][0])
+		quest.completequest(223)
+		v=items.useitem('player.box.gambling.3')
+		if v['data']['itemBeans'][0]['id'] in itemsarray:
+			result.append(v['data']['itemBeans'][0])
+		v=items.useitem('player.box.gambling.3')
+		if v['data']['itemBeans'][0]['id'] in itemsarray:
+			result.append(v['data']['itemBeans'][0])
+		v=items.useitem('player.box.gambling.3')
+		if v['data']['itemBeans'][0]['id'] in itemsarray:
+			result.append(v['data']['itemBeans'][0])
+		for g in prioritylist:
+			i=0
+			while i<(len(result)):
+				if result[i]['id']==('player.box.gambling.'+g):
+					try:
+						x.client.sendmessage('trade.newTrade',{'castleId':castleid,'price':'0.001','tradeType':1,'amount':result[i]['count'],'resType':itemsarray[result[i]['id']]})
+						res=x.responsehandler('trade.newTrade')
+						callback("8||SUCCESSFULTRADE"+g+" "+str(result[i]['count']))
+						i=i+1
+						continue
+					except Exception as e:
+						if e.args[0]==-1:
+							time.sleep(2)
+							continue
+						i=i+1
+				i=i+1
+		return x
+	except:
+		try:
+			x.close()
+		except:
+			pass
+def _stamulet(server,prioritylist,proxy=None,proxytype='HTTP',useclient=None,callback=None,checksource=None,timeout=30,totalrunning=None):
+	x=0
+	useclient=None
+	while x!=None:
+		if useclient!=None:
+			useclient.registered=False
+		if checksource!=None:
+			if checksource.killsignal:
+				break
+		x=createacc(server,prioritylist,useclient,proxy,proxytype,callback,timeout=timeout)
+		useclient=x
+	totalrunning[0]-=1
+def startmarket(server,prioritylist,useclient=None,proxy=None,proxytype='HTTP',callback=None,checksource=None,timeout=30,totalconn=1):
+	totalrunning=[0]
+	for i in range(0,totalconn):
+		totalrunning[0]+=1
+		while True:
+			try:
+				Thread(target=_stamulet,args=(server,prioritylist,proxy,proxytype,useclient,callback),kwargs={'checksource':checksource,'totalrunning':totalrunning,'timeout':timeout}).start()
+				break
+			except:
+				time.sleep(.3)
+	while True:
+		if checksource!=None:
+			if checksource.killsignal:
+				if totalrunning[0]>0:
+					time.sleep(.3)
+					continue
+				callback("|NOKILL|")
+				return
+		if totalrunning[0]==0:
+			callback("|KILL|")
+			return
+		time.sleep(.3)
---
+++ proxy-version/marketproxy.py
@@ -0,0 +1,131 @@
+from GUIMARKET import *
+from proxymanager import ProxyManager
+import wx
+import json
+import sys
+import os
+from threading import Thread
+import time
+from market import *
+app=wx.App()
+frame1=MyFrame1(None)
+frame2=ProxyManager()
+if os.path.exists('tradeconfig.json'):
+	try:
+		frame1.m_textCtrl905.SetValue("30")
+		config=json.loads(open('tradeconfig.json','r').read())
+		if 'server' in config:
+			frame1.m_textCtrl1.SetValue(config['server'])
+		if 'resource1' in config:
+			frame1.m_choice6.SetSelection(config['resource1'])
+		if 'resource2' in config:
+			frame1.m_choice7.SetSelection(config['resource2'])
+		if 'resource3' in config:
+			frame1.m_choice8.SetSelection(config['resource3'])
+		if 'resource4' in config:
+			frame1.m_choice9.SetSelection(config['resource4'])
+		if 'totalconnections' in config:
+			frame1.m_textCtrl4.SetValue(str(config['totalconnections']))
+		if 'maxchecking' in config:
+			frame1.m_textCtrl5.SetValue(str(config['maxchecking']))
+		if 'usebroker' in config:
+			frame1.m_checkBox1.SetValue(config['usebroker'])
+		if 'maxperproxy' in config:
+			frame1.m_textCtrl95.SetValue(str(config['maxperproxy']))
+		if "timeout" in config:
+			frame1.m_textCtrl905.SetValue(str(config['timeout']))
+	except:
+		pass
+def finishdatahandler(value):
+	global totalfoodsold,totalwoodsold,totalironsold,totalstonesold,linecount
+	if (value[:15]!='SUCCESSFULTRADE'):
+		linecount+=1
+		if linecount>=100:
+			wx.CallAfter(frame1.m_textCtrl3.SetValue,'')
+			linecount=0
+		wx.CallAfter(frame1.m_textCtrl3.AppendText,(value+"\n"))
+		return
+	value=value[15:]
+	value1=value.split(' ')
+	if value1[0]=='food':
+		totalfoodsold+=int(value1[1])
+		wx.CallAfter(frame1.m_staticText17.SetLabel,("Food Sold : "+format(totalfoodsold,',')))
+		return
+	if value1[0]=='wood':
+		totalwoodsold+=int(value1[1])
+		wx.CallAfter(frame1.m_staticText23.SetLabel,("Wood Sold : "+format(totalwoodsold,',')))
+		return
+	if value1[0]=='stone':
+		totalstonesold+=int(value1[1])
+		wx.CallAfter(frame1.m_staticText24.SetLabel,("Stone Sold : "+format(totalstonesold,',')))
+		return
+	if value1[0]=='iron':
+		totalironsold+=int(value1[1])
+		wx.CallAfter(frame1.m_staticText25.SetLabel,("Iron Sold : "+format(totalironsold,',')))
+		return
+def showdetails(event):
+	frame2.Show()
+	frame2.SetFocus()
+frame1.m_button21.Bind(wx.EVT_BUTTON,showdetails)
+def checkstopped():
+	if frame2.killsignal==True:
+		wx.CallLater(1000,checkstopped)
+		return
+	frame1.m_button2.Enable()
+def onstop():
+	if frame1.m_button2.GetLabel()=="Start":
+		return
+	frame2.stop()
+	frame1.m_button2.SetLabel("Start")
+	frame1.m_button2.Disable()
+	checkstopped()
+def exectrade(event):
+	global linecount
+	linecount=0
+	if frame1.m_button2.GetLabel()=="Stop":
+		onstop()
+		return
+	global totalironsold,totalwoodsold,totalstonesold,totalfoodsold
+	frame1.m_button2.SetLabel("Stop")
+	server=frame1.m_textCtrl1.GetValue()
+	resource1=frame1.m_choice6.GetSelection()
+	resource2=frame1.m_choice7.GetSelection()
+	resource3=frame1.m_choice8.GetSelection()
+	resource4=frame1.m_choice9.GetSelection()
+	try:
+		totalconnections=int(frame1.m_textCtrl4.GetValue())
+		maxchecking=int(frame1.m_textCtrl5.GetValue())
+		maxperproxy=int(frame1.m_textCtrl95.GetValue())
+		timeout=int(frame1.m_textCtrl905.GetValue())
+		if maxperproxy<=0:
+			raise
+	except:
+		frame1.m_button2.SetLabel("Start")
+		return
+	usebroker=frame1.m_checkBox1.GetValue()
+	try:
+		d=open('tradeconfig.json','w')
+		e={'server':server,'resource1':resource1,'timeout':timeout,'usebroker':usebroker,'resource2':resource2,'resource3':resource3,'resource4':resource4,'totalconnections':totalconnections,'maxchecking':maxchecking,'maxperproxy':maxperproxy}
+		json.dump(e,d)
+		d.close()
+	except:
+		pass
+	frame2.MAX_CHECKS=maxchecking
+	frame2.MAX_PROCESS=totalconnections
+	totalironsold=0
+	totalstonesold=0
+	totalwoodsold=0
+	totalfoodsold=0
+	if usebroker:
+		t=Thread(target=frame2.startbroker)
+		t.daemon=True
+		t.start()
+	frame2.startchecker()
+	frame2.runprocess(startmarket,server,[resource1,resource2,resource3,resource4],totalconn=maxperproxy,timeout=timeout,callback=finishdatahandler)
+def onclose(event):
+	onstop()
+	sys.exit()
+frame1.m_button2.Bind(wx.EVT_BUTTON,exectrade)
+frame1.Bind(wx.EVT_CLOSE,onclose)
+frame1.Show()
+app.MainLoop()
